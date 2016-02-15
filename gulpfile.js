bf4f23ac34d48ea29c14e18d9f57658c3f0adaeb
Constantine Karnaukhov <genteelknight@gmail.com>
2016-02-16 01:01:39 +0400

    Gulp tasks with config

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,30 @@
+// =====================================
+// CONFIG
+// =====================================
+var config = {
+  cssPreprocessor: "Sass",
+  preprocessorOptions: {}
+};
+
+// PATHS
+// =====================================
+config.source = {};
+config.source.root = "src/";
+config.source.styles = config.source.root+"styles/";
+config.source.scripts = config.source.root+"scripts/";
+config.source.mainStyle = config.source.styles+"main.scss";
+
+config.result = {};
+config.result.root = "assets/";
+config.result.styles = config.result.root+"css/";
+config.result.scripts = config.result.root+"js/";
+config.result.mainStyle = config.result.styles+"main.css";
+
 require('es6-promise').polyfill();
 // =====================================
 // DEPENDENCIES
 // =====================================
 var gulp = require('gulp');
-var less = require('gulp-less');
-var sass = require('gulp-sass');
 var path = require('path');
 var autoprefixer = require('gulp-autoprefixer');
 var csscomb = require('gulp-csscomb');
@@ -13,66 +33,56 @@ var rename = require('gulp-rename');
 var sourcemaps = require('gulp-sourcemaps');
 var argv = require('yargs').argv;
 
-// =====================================
-// PATHS
-// =====================================
-var source = {
-  root: "src/",
-  styles: this.root+"styles/",
-  scripts: this.root+"scripts/",
-  mainStyle: this.styles+"main.scss"
-};
-
-var result = {
-  root: "assets/",
-  styles: this.root+"css/",
-  scripts: this.root+"js/",
-  mainStyle: this.styles+"main.css"
-};
+var preprocessor;
+if (config.cssPreprocessor == "Sass") {
+  preprocessor = require('gulp-sass');
+} else {
+  preprocessor = require('gulp-less');
+}
 
 // =====================================
 // TASKS
 // =====================================
 // style compile
 gulp.task('styler', function() {
-  var src = source.mainStyle;
+  var src = config.source.mainStyle;
 
   gulp.src(src)
     .pipe(sourcemaps.init())
-    .pipe(sass())
-    .pipe(sourcemaps.write('.', {includeContent: false, sourceRoot: '../../'+source.styles}))
-    .pipe(gulp.dest(result.styles));
+    .pipe(preprocessor(config.preprocessorOptions))
+    .pipe(sourcemaps.write('.', {includeContent: false, sourceRoot: '../../'+config.source.styles}))
+    .pipe(gulp.dest(config.result.styles));
 });
 
 // add prefixes
 gulp.task('prefixer', function () {
-  var src = result.mainStyle;
+  var src = config.result.mainStyle;
 
   gulp.src(src)
     .pipe(autoprefixer({
       browsers: ['Android 2.3', 'Android >= 4', 'Chrome >= 20', 'Firefox >= 24', 'Explorer >= 8', 'iOS >= 6', 'Opera >= 12', 'Safari >= 6'],
       cascade: false
     }))
-    .pipe(gulp.dest(result.styles));
+    .pipe(gulp.dest(config.result.styles));
 });
 
 // ccscomb
 gulp.task('combing', function() {
-  var src = result.mainStyle;
+  var src = config.result.mainStyle;
 
   gulp.src(src)
     .pipe(csscomb())
-    .pipe(gulp.dest(result.styles));
+    .pipe(gulp.dest(config.result.styles));
 });
 
 // minify css
 gulp.task('minifying', function() {
-  var src = result.mainStyle;
+  var src = config.result.mainStyle;
 
   gulp.src(src)
     .pipe(cssnano())
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest(result.styles));
+    .pipe(gulp.dest(config.result.styles));
 });
 
 // Build styles
@@ -82,7 +92,7 @@ gulp.task('build-css', ['styler', 'prefixer', 'combing', 'minifying']);
 gulp.task('watch', function() {
   console.log('Start watching for styles');
 
-  var watcher = gulp.watch(source.styles+'**', ['styler']);
+  var watcher = gulp.watch(config.source.styles+'**', ['styler']);
   watcher.on('change', function(event) {
     console.log('Stylesheet ' + event.path + ' was ' + event.type + ', recompile...');
   });
